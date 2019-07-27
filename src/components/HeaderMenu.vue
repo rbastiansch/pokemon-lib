5f7ecb62c69f6bf2806de4b3e2d076f6d420413a
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2019-07-27 13:14:16 -0300

    remove non used stuff, create basic components

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Pokemon lib</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderMenu',
+};
+</script>
---
+++ src/components/ListItems.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>List of pokemons</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListItems',
+};
+</script>
